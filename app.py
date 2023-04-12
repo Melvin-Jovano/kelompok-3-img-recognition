dadd5bc53c78cfb94cd026e0708d2ed260a853fe
KevinFrederick <kevin55622@gmail.com>
2023-04-12 13:14:59 +0700

    Predict All Input Img

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import os
 import uuid
+from common.ai import preprocess, predict
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,11 +11,14 @@ def form():
 
 @app.route('/', methods=['POST'])
 def submit():
+    # imgList = []
     for file in request.files.getlist('images'):
         if file.content_type.startswith('image/'):
             fileName = file.filename.split('.')
             img = str(uuid.uuid4()) + '.' + fileName[len(fileName) - 1]
             file.save(os.path.join('img', img))
+            # imgList.append(img)
+    predict(preprocess(os.listdir("img")), os.listdir("img"))
     
     return 'Files uploaded successfully'
 
--- common/ai.py
+++ common/ai.py
@@ -3,22 +3,37 @@
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications import VGG16
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.applications import VGG16
+import keras.utils as image
+from keras.applications import VGG16
 
 # Load pre-trained VGG16 model
 model = VGG16(weights='imagenet')
 
 # Load and preprocess image
-img_path = '../img/cat_1.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = tf.keras.applications.vgg16.preprocess_input(x)
+def preprocess(imgList):
+    processedImg = []
+    for i in imgList:
+        img_path = f'img/{i}'
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = tf.keras.applications.vgg16.preprocess_input(x)
+        processedImg.append(x)
+    return processedImg
 
 # Predict image class
-preds = model.predict(x)
-print('Predicted:', tf.keras.applications.vgg16.decode_predictions(preds, top=3)[0])
+def predict(processedImg, imgList):
+    preds = model.predict(np.vstack(processedImg))
+    decode = tf.keras.applications.vgg16.decode_predictions(preds, top=3)
+    result = []
+    for i in range(len(processedImg)):
+        predicted = {"Image" : imgList[i], "Predicted" : decode[i]}
+        print(f"Img : {predicted['Image']}\nPredicted : {predicted['Predicted']}")
+        print("")
+        result.append(predicted)
+    return result
 
 # Output: 
 # array of: (class_indentifier, object_prediction, confidence_level)
